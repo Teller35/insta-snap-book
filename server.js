5a06a17892463f0ef7f909956a9f0d1aba8b731e
Teller35 <tellerwetzel@yahoo.com>
2021-07-07 15:48:17 -0600

    starter files and server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require("./routes"));
+
+// mongoose function
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/insta_snap_book", {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
